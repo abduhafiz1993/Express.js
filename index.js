62f92a15fe026c5df17d2ff68680b252c115963a
Abdulhafiz Muhammed <muhammedabdulhafiz1993@gmail.com>
2024-09-03 23:39:58 +0300

    handeling get req with parametres

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const express = require('express');
 const app = express();
 
 const users = [{
-    id: '1',
+    id: 1,
     nameofuser: "Abdu" 
 },
 {
-id: '2',
+id: 2,
 nameofuser: "Abdu2" 
 },
 {
-id: '3',
+id: 3,
 nameofuser: "Abdu3" 
 },
 {
-id: '4',
+id: 4,
 nameofuser: "Abdu4" 
 }]
 
@@ -28,6 +28,17 @@ app.get('/users', (req, res)=> {
     res.send(users);
 })
 
+app.get('/users/:id', (req, res)=> {
+    const user = users.find(c=>c.id === parseInt(req.params.id));
+
+    if(!user) return res.status(404).send('The user with the given Id was not eixist')
+    
+    res.send(user);
+})
+
+app.post('/users', (req, res)=> {
+    
+});
 const port = process.env.PORT||3000;
 
 app.listen(port, ()=>console.log(`listening on ${port}`));
