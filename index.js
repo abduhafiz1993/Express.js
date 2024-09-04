a874694f891d410c9cf8555edc7604992424e8d2
Abdulhafiz Muhammed <muhammedabdulhafiz1993@gmail.com>
2024-09-04 23:20:21 +0300

    adding prisma and post function

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 const express = require('express');
 const Joi = require('joi');
+const { PrismaClient } = require('@prisma/client')
 
 const app = express();
+const prisma = new PrismaClient();
+
 
 app.use(express.json());
 
-const users = [{
-    id: 1,
-    nameofuser: "Abdu" 
-},
-{
-id: 2,
-nameofuser: "Abdu2" 
-},
-{
-id: 3,
-nameofuser: "Abdu3" 
-},
-{
-id: 4,
-nameofuser: "Abdu4" 
-}]
+const userSchema = Joi.object({
+    name: Joi.string().min(3),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+  });
 
-app.get('/', (req, res)=> {
-    res.send('Hello World');
-})
+
+
+
+app.post('/users', async (req, res)=> {
+    try {
+        const { error, value } = userSchema.validate(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+    
+        const user = await prisma.user.create({
+          data: value,
+        });
+        res.status(201).json(user);
+      } catch (err) {
+        res.status(500).json({ error: err.message });
+      }
+});
+
 
 
 app.get('/users', (req, res)=> {
@@ -38,24 +44,6 @@ app.get('/users/:id', (req, res)=> {
     
     res.send(user);
 })
-
-app.post('/users', (req, res)=> {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-
-    const { error } = schema.validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-  
-    const user = {
-        id: users.length+1,
-        nameofuser: req.body.name
-    }
-
-    users.push(user);
-    res.send(user);
-});
-
 app.put('/users/:id',(req, res)=>{
     const user = users.find(c=>c.id === parseInt(req.params.id));
 
@@ -80,6 +68,8 @@ app.delete('/users/:id', (req, res)=>{
 
     const index  = users.indexOf(user);
     users.splice(index, 1);
+
+    res.send(users);
 })
 
 const port = process.env.PORT||3000;
